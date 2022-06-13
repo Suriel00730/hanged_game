10617f2b99aa5d6d313e6cc3fd5c1b61578da098
Suriel00730 <suriel00730is@gmail.com>
2022-06-13 09:44:07 -0400

    creando el teclado para diseño de smartphone

--- assets/JS/juego.js
+++ assets/JS/juego.js
@@ -14,8 +14,27 @@ munieco_ahorcado.push(pierna_derecha);
 
 const btn_nuevo_juego = document.querySelector(".btn_nuevo_juego");
 const btn_desistir = document.querySelector(".btn_desistir");
+const contenedor_teclado = document.querySelector(".contenedor_teclado");
+
+
+contenedor_teclado.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // var presionada = event.target.innerHTML;
+    // console.log(presionada);
+
+    if (contenedor_juego.classList == "contenedor_juego oculto") return;
+
+    if (palabras_presionadas.includes(event.target.textContent)) return;
+
+    insertarLetra(event.target.innerHTML.toUpperCase());
+    palabras_presionadas.push(event.target.innerHTML.toUpperCase());
+    mostrarMensaje();
+});
 
 document.addEventListener("keydown", function (event) {
+
+    // recibirLetra(event.key);
 
     if (contenedor_juego.classList == "contenedor_juego oculto") return;
 
@@ -43,3 +62,15 @@ btn_desistir.addEventListener("click", function (event) {
     contenedor_juego.classList.add("oculto");
     home.classList.remove("oculto");
 });
+
+function recibirLetra(letra) {
+
+    if (contenedor_juego.classList == "contenedor_juego oculto") return;
+
+    if (palabras_presionadas.includes(letra)) return;
+
+    if (!verificarLetra(letra)) return;
+    insertarLetra(letra.toUpperCase());
+    palabras_presionadas.push(letra.toUpperCase());
+    mostrarMensaje();
+}
